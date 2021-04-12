48c5ad47e68aa8f5916c335755f822946be0afae
aryyawijaya <aryyawijaya@gmail.com>
2021-04-12 13:39:38 +0700

    add and configure static file

--- perpus/perpus/settings.py
+++ perpus/perpus/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR, 'static', # untuk menyimpang file static (css, js, png) di folder static
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
