d3d884c78baafe1700cfca0e114793573deff18d
aryyawijaya <aryyawijaya@gmail.com>
2021-04-12 16:21:54 +0700

    add models

--- perpus/perpustakaan/models.py
+++ perpus/perpustakaan/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Buku(models.Model):
+    judul = models.CharField(max_length=50)
+    penulis = models.CharField(max_length=40)
+    penerbit = models.CharField(max_length=40)
+    jumlah = models.IntegerField(null=True)
+
+    def __init__(self):
+        return self.judul # yg direturn harus tipe data string
