13cb83b0a44e030559d9f8df2fdec43a250a6a4e
aryyawijaya <aryyawijaya@gmail.com>
2021-04-13 10:15:42 +0700

    setup widget form with bootstrap

--- perpus/perpustakaan/forms.py
+++ perpus/perpustakaan/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from perpustakaan.models import Buku, Kategori
 
 class FormBuku(ModelForm):
@@ -8,7 +9,21 @@ class FormBuku(ModelForm):
         # ['judul', 'penulis'] --> hanya field tertentu yg muncul
         # exlude = ['judul'] --> semua field muncul kecuali judul
 
+        widgets = {
+            'judul': forms.TextInput({'class':'form-control'}),
+            'penulis': forms.TextInput({'class':'form-control'}),
+            'penerbit': forms.TextInput({'class':'form-control'}),
+            'jumlah': forms.NumberInput({'class':'form-control'}),
+            'kategori_id': forms.Select({'class':'form-control'}),
+
+        }
+
 class FormKategori(ModelForm):
     class Meta:
         model = Kategori
         fields = '__all__'
+
+        widgets = {
+            'nama': forms.TextInput({'class':'form-control'}),
+            'keterangan':forms.Textarea({'class':'form-control'}),
+        }
